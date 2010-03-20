732291a00fc65de2ecc1fac48a4b66d5c378f54d
Matthew Leach <matthew@demon.leach.local>
2010-03-20 03:04:29 +0000

    Added news code stub.

---
+++ src/sounds/news.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+#
+# news - This object represents a news cart type sound.
+# It will also hold data relevent to the posistion, colour and type of cart.
+#
+# This file is part of Fusic.
+#
+# Fusic is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Fusic is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Fusic.  If not, see <http://www.gnu.org/licenses/>.
+
+class news(cart):
+
+	# Constructor
+	def __init__(self):
+		# TODO - Add MySQL instructions here to set member variables.
+		cart.__init__(self, 0, 0) # TODO - Ensure that the correct cart is
+		# constructed here.
+
+	# Class properties
+	@property
+	def date_time(self):
+		return 0
+
+	@property
+	def presenter(self):
+		return 0
