7230bcc5685d74e86b11504a212cddaac5e84d21
Matthew Leach <matthew@demon.leach.local>
2010-03-20 02:46:11 +0000

    Added sweeper code stub.

---
+++ src/sounds/sweeper.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+#
+# sweeper - This object represents a sweeper type sound.
+#
+# This file is part of Fusic.
+#
+# Fusic is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Fusic is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Fusic.  If not, see <http://www.gnu.org/licenses/>.
+
+class sweeper(sound):
+
+	# Constructor
+	def __init__(self, fileID):
+		"""
+		Create a new sweeper with a given fileID (int).
+		"""
+		# TODO - Add MySQL instructions here to set member variables.
+		pass
