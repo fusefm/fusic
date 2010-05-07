90f5e0a0d4aa164700222c4835fdff539bb42b96
Matthew Leach <matt@fusefm.co.uk>
2010-05-08 00:58:00 +0100

    Added the song class.

---
+++ src/sounds/song.cpp
@@ -0,0 +1,87 @@
+/*
+    fusic - Radio playout software
+    Copyright (C) 2010  Matthew Leach
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+// Local Includes:
+#include "song.h"
+
+// Qt Includes:
+#include <QSqlQuery>
+#include <QSqlRecord>
+#include <QVariant>
+
+// Public getter implimentation.
+
+QString song::getAlbum()
+{
+    mutex.lock();
+    mutex.unlock();
+    return Album;
+}
+
+QString song::getArtist()
+{
+    mutex.lock();
+    mutex.unlock();
+    return Artist;
+}
+
+double song::getIntro()
+{
+    mutex.lock();
+    mutex.unlock();
+    return Intro;
+}
+
+QString song::getGenre()
+{
+    mutex.lock();
+    mutex.unlock();
+    return Genre;
+}
+
+// Do extra setup for extended properties.
+bool song::extraSetup(QSqlDatabase &db, int soundID)
+{
+    // Ensure we have an active database connection.
+    if(!db.isOpen())
+        return false;
+
+    QSqlQuery query("SELECT * FROM songs WHERE Sound_ID = ?");
+    query.bindValue(0, soundID);
+    query.exec();
+
+    // Ensure we have a result.
+    if(!query.first())
+        return false;
+
+    // Get field IDs.
+    int fIDGenre = query.record().indexOf("genre");
+    int fIDArtist = query.record().indexOf("artist");
+    int fIDAlbum = query.record().indexOf("album");
+    int fIDIntro = query.record().indexOf("intro");
+
+    // Get the data.
+    Genre = query.value(fIDGenre).toString();
+    Artist = query.value(fIDArtist).toString();
+    Album = query.value(fIDAlbum).toString();
+    Intro = query.value(fIDIntro).toDouble();
+
+    // All is good.
+    return true;
+}
