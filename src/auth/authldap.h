8fc04a63739d59afad4744eadbf1d5b5c317e2e8
Matthew Leach <matt@fusefm.co.uk>
2010-05-12 11:11:19 +0100

    Added ldap CMake module and auth class.

---
+++ src/auth/authldap.h
@@ -0,0 +1,34 @@
+/*
+    fusic - radio playout software.
+    Copyright (C) 2010  Matthew Leach
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#ifndef AUTHLDAP_H
+#define AUTHLDAP_H
+
+#include <../../home/matthew/Documents/Development/fusic/src/auth/authbase.h>
+
+
+class authLDAP : public authBase
+{
+
+public:
+    virtual void doAuth();
+    virtual const QDialog* getsettingsDialog(QWidget* parent);
+};
+
+#endif // AUTHLDAP_H
