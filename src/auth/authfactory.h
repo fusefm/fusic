f18bdeccec9ac8c1e0b30d71884fca1274859e15
Matthew Leach <matt@fusefm.co.uk>
2010-08-09 16:36:06 +0100

    Added a authFactory class to the auth module.

---
+++ src/auth/authfactory.h
@@ -0,0 +1,34 @@
+/*
+    fusic - radio playout software.
+    Copyright (C) 2010 Matthew Leach
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#ifndef AUTHFACTORY_H
+#define AUTHFACTORY_H
+
+// Local includes:
+#include "authbase.h"
+
+//Qt Includes:
+#include <QList>
+
+class authFactory
+{
+  static QList<authBase*> getAuthModules();
+};
+
+#endif // AUTHFACTORY_H
